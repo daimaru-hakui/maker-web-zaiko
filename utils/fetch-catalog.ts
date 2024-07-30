59f62fd6115b3bf112d6cd251b09a40de3d53f87
daimaru-hakui <dh.mukai.dh@gmail.com>
2024-07-30 15:27:14 +0900

    apikey microcms

--- utils/fetch-catalog.ts
+++ utils/fetch-catalog.ts
@@ -5,14 +5,12 @@ export const fetchCatalog = async (id: string) => {
   const res = await axios
     .get(`${url}/${id}`, {
       headers: {
-        "X-MICROCMS-API-KEY":
-          process.env.X_MICROCMS_API_KEY ||
-          "3c62454d-9a98-4e3d-aee1-d337c3bbdf7e",
+        "X-MICROCMS-API-KEY": process.env.X_MICROCMS_API_KEY,
       },
     })
-    .catch((e:unknown) => {
+    .catch((e: unknown) => {
       console.log(e instanceof Error ? e.message : "エラーが発生しました");
-      return null
+      return null;
     });
   return res?.data || null;
 };
