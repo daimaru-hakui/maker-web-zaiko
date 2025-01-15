e9aa6791ccf8bca584e22aa5b2f9d83bdd4dd035
daimaru-hakui <dh.mukai.dh@gmail.com>
2025-01-15 18:37:43 +0900

    redirect daimaru-hakui

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import { redirect } from 'next/navigation'
 
 
 export default async function Home() {
-  redirect('/daimaru')
+  redirect('/daimaru-hakui')
   return (
     <main className="flex flex-col items-center justify-between overflow-hidden">
       大丸白衣
