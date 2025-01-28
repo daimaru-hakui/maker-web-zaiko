c1f9b16654fe4ee7e3b10eebbd365991d2fba057
daimaru-hakui <dh.mukai.dh@gmail.com>
2025-01-28 09:27:37 +0900

    update maker modify

--- app/admin/admin-table-td.tsx
+++ app/admin/admin-table-td.tsx
@@ -1,7 +1,7 @@
 "use client";
 import { db } from "@/libs/firebase";
 import { Switch, Td } from "@chakra-ui/react";
-import { doc, updateDoc } from "firebase/firestore";
+import { doc, getDoc, setDoc, updateDoc } from "firebase/firestore";
 import React, { FC } from "react";
 
 type Props = {
@@ -14,6 +14,13 @@ export const AdminTableTd: FC<Props> = ({ uid, isMaker, title }) => {
 
   const updateMaker = async () => {
     const makerDoc = doc(db, "users", `${uid}`, "permissions", "makers");
+    const docSnapshot = await getDoc(makerDoc);
+    if (!docSnapshot.exists()) {
+      await setDoc(makerDoc, {
+        [title]: !isMaker,
+      })
+      return
+    }
     updateDoc(makerDoc, {
       [title]: !isMaker,
     });
